7cc4edee997e234b0a84a94bcb9222ef7e67c051
Subodh Katna <subodhkatna@gmai.com>
2025-11-09 21:31:21 +0530

    done with security group

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,        // ✅ change to any port you want
+    strictPort: true,  // optional (prevents auto port switching)
+  }
 })
